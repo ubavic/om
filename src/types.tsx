81d3fe10b67b866f7492f9ef2443bd10786efcaf
Nikola Ubavic <53820106+ubavic@users.noreply.github.com>
2024-01-27 17:06:11 +0100

    Use `Exclude` utility type

--- src/types.tsx
+++ src/types.tsx
@@ -1,8 +1,6 @@
 type Color = 'Black' | 'Brown' | 'Red' | 'Orange' | 'Yellow' | 'Green' | 'Blue' | 'Violet' | 'Grey' | 'White' | 'Gold' | 'Silver'
 
-type Without_<T, U extends Color> = T extends U ? never : T
-
-type Without<U extends Color> = Without_<Color, U>
+type Without<U extends Color> = Exclude<Color, U>
 
 type NumberOfBands = 3 | 4 | 5 | 6
 
@@ -10,7 +8,7 @@ type Digit = Without<'Gold' | 'Silver'>
 
 type Multiplier = Color
 
-type Tolerance = Without<'White'>
+type Tolerance = Without<'Black' | 'Orange' | 'Yellow' | 'Grey' | 'White'>
 
 type TemperatureCoefficient = Without<'White' | 'Gold' | 'Silver'>
 
